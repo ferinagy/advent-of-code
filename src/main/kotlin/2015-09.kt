54071710fc188402784d4baf8a0eb2558ca6a8a0
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2021-12-02 15:57:58 +0100

    Add cache to TSP graph

--- src/main/kotlin/2015-09.kt
+++ src/main/kotlin/2015-09.kt
@@ -33,6 +33,10 @@ private class TspGraph {
 
     private val edges = mutableMapOf<String, MutableMap<String, Int>>()
 
+    private data class CacheEntry(val node: String, val remaining: Set<String>)
+
+    private val cache = mutableMapOf<CacheEntry, Progress>()
+
     fun addBidirectionalEdge(from: String, to: String, weight: Int) {
         val e1 = edges.getOrPut(from) { mutableMapOf() }
         val e2 = edges.getOrPut(to) { mutableMapOf() }
@@ -50,18 +54,23 @@ private class TspGraph {
     }
 
     private fun solveTsp(node: String, remaining: Set<String>, comparator: Comparator<Int>): Progress {
+        val cacheKey = CacheEntry(node, remaining)
+        if (cacheKey in cache) return cache[cacheKey]!!
+
         if (remaining.isEmpty()) {
-            return Progress(node, setOf(node), listOf(node), 0)
+            return Progress(node, setOf(node), listOf(node), 0).also { cache[cacheKey] = it }
         }
 
         val subTasks = remaining.map {
             solveTsp(it, remaining - it, comparator)
         }
 
-        return subTasks.map {
+        val result = subTasks.map {
             val dist = edges[it.end]!![node]!!
             Progress(node, it.visited + node, it.path + node, dist + it.dist)
         }.minWithOrNull { o1, o2 -> comparator.compare(o1.dist, o2.dist) }!!
+
+        return result.also { cache[cacheKey] = it }
     }
 
     data class Progress(val end: String, val visited: Set<String>, val path: List<String>, val dist: Int)
