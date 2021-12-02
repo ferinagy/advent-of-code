d107b4da2bad9fac8b7da0a2eb71bcc6d4dcd8be
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2021-12-02 12:55:30 +0100

    Update template

--- src/main/kotlin/Template.kt
+++ src/main/kotlin/Template.kt
@@ -1,8 +1,20 @@
 fun main(args: Array<String>) {
     println("Part1:")
+    println(part1(testInput1))
+    println(part1(input))
 
     println()
     println("Part2:")
+    println(part2(testInput1))
+    println(part2(input))
+}
+
+private fun part1(input: String): Int {
+    return 0
+}
+
+private fun part2(input: String): Int {
+    return 0
 }
 
 private const val testInput1 = """"""
