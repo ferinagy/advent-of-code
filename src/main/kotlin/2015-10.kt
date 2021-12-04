4c36fc338b532907ce062f00c94884da6c4478f2
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2021-12-04 12:31:37 +0100

    Solve 2015/10

---
+++ src/main/kotlin/2015-10.kt
@@ -0,0 +1,46 @@
+fun main(args: Array<String>) {
+    println("Part1:")
+    println(part1(input))
+
+    println()
+    println("Part2:")
+    println(part2(input))
+}
+
+private fun part1(input: String): Int {
+    var x = input
+    repeat(40) {
+        x = lookAndSay(x)
+    }
+
+    return x.length
+}
+
+private fun part2(input: String): Int {
+    var x = input
+    repeat(50) {
+        x = lookAndSay(x)
+    }
+
+    return x.length
+}
+
+private fun lookAndSay(input: String): String {
+    var runStart = 0
+    var runEnd = 0
+
+    return buildString {
+        while (runStart < input.length) {
+            while (runEnd < input.length && input[runStart] == input[runEnd]) runEnd++
+
+            append((runEnd - runStart))
+            append(input[runStart])
+
+            runStart = runEnd
+        }
+    }
+}
+
+private const val testInput1 = """111221"""
+
+private const val input = """1113222113"""
