4b59c7fc242dcfc9eee7c8e02417e7945be1a479
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2021-12-13 16:03:34 +0100

    Solve 2015/18

--- src/main/kotlin/com/github/ferinagy/adventOfCode/Coord2D.kt
+++ src/main/kotlin/com/github/ferinagy/adventOfCode/Coord2D.kt
@@ -1,8 +1,27 @@
 package com.github.ferinagy.adventOfCode
 
+@JvmInline value class Coord2DValue(val value: Long) {
+}
+
 data class Coord2D(val x: Int, val y: Int) {
 
     companion object {
         fun parse(input: String) = input.split(",").let { (x, y) -> Coord2D(x.toInt(), y.toInt()) }
     }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    fun adjacent(includeDiagonals: Boolean): List<Coord2D> = buildList {
+        setOf(-1 to 0, 0 to -1, 1 to 0, 0 to 1).forEach {
+            this += copy(x = x + it.first, y = y + it.second)
+        }
+        if (includeDiagonals) {
+            setOf(-1 to -1, -1 to 1, 1 to -1, 1 to 1).forEach {
+                this += copy(x = x + it.first, y = y + it.second)
+            }
+        }
+    }
+
+    operator fun plus(other: Coord2D) = copy(x = x + other.x, y = y + other.y)
 }
+
+fun List<Coord2D>.filterIn(xRange: IntRange, yRange: IntRange) = filter { it.x in xRange && it.y in yRange }
