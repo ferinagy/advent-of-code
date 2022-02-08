6ec7c487986a59c4568685f4b66098826fe7f288
Frantisek Nagy <frantisek.nagy.ml@gmail.com>
2022-02-08 09:53:55 +0100

    Improve 2016/11

--- src/main/kotlin/com/github/ferinagy/adventOfCode/Lists.kt
+++ src/main/kotlin/com/github/ferinagy/adventOfCode/Lists.kt
@@ -1,5 +1,7 @@
 package com.github.ferinagy.adventOfCode
 
+import java.util.*
+
 fun <T> List<List<T>>.transpose(): List<List<T>> {
     if (isEmpty()) return emptyList()
 
@@ -30,4 +32,22 @@ fun <T> Set<T>.subSets(size: Int): Set<Set<T>> {
         val set: Set<Set<T>> = (this - it).subSets(size - 1)
         set.map { s -> s + it }
     }.toSet()
+}
+
+fun BitSet.subSets(size: Int): Set<BitSet> {
+    if (size == 0) return setOf(BitSet())
+
+    val sub = subSets(size - 1)
+
+    return buildSet {
+        var index = nextSetBit(0)
+        while (index != -1) {
+            sub.forEach {
+                if (!it.get(index)) {
+                    this += it.clone().let { it as BitSet }.also { it.set(index) }
+                }
+            }
+            index = nextSetBit(index + 1)
+        }
+    }
 }
--- src/main/kotlin/com/github/ferinagy/adventOfCode/aoc2016/2016-11.kt
+++ src/main/kotlin/com/github/ferinagy/adventOfCode/aoc2016/2016-11.kt
@@ -2,44 +2,78 @@ package com.github.ferinagy.adventOfCode.aoc2016
 
 import com.github.ferinagy.adventOfCode.subSets
 import java.util.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
     println("Part1:")
-    println(part1(testInput1))
-    println(part1(input))
+    val time1 = measureTimeMillis {
+        println(part1(testInput1))
+        println(part1(input))
+    }
+    println("took $time1")
 
     println()
     println("Part2:")
-    println(part2(testInput1))
-    println(part2(input))
+    val time2 = measureTimeMillis {
+        println(part2(testInput1))
+        println(part2(input))
+    }
+    println("took $time2")
 }
 
-private fun part1(input: RtgState): Int {
-    return searchGraph(start = input, isDone = RtgState::isDone, nextSteps = RtgState::nextSteps, computePath = true)
+private fun part1(input: String): Int {
+    return searchGraph(
+        start = RtgState.parse(input),
+        isDone = RtgState::isDone,
+        nextSteps = RtgState::nextSteps,
+    )
 }
 
-private fun part2(input: RtgState): Int {
-    val nextInput = input.copy(
-        floors = input.floors.mapIndexed { index, floor ->
-            if (index == 0) {
-                floor + listOf(
-                    RtgState.Component.Chip("El"),
-                    RtgState.Component.Generator("El"),
-                    RtgState.Component.Chip("Di"),
-                    RtgState.Component.Generator("Di"),
-                )
-            } else floor
+private fun part2(input: String): Int {
+    val rtgState = RtgState.parse(input)
+    val nextInput = rtgState.copy(
+        elementCount = rtgState.elementCount + 2,
+        floors = rtgState.floors.mapIndexed { index, bitSet ->
+            if (index != 0) bitSet else bitSet.also { it.set(rtgState.elementCount * 2, rtgState.elementCount * 2 + 4) }
         }
     )
     return searchGraph(
         start = nextInput,
         isDone = RtgState::isDone,
         nextSteps = RtgState::nextSteps,
-        computePath = true
     )
 }
 
-private data class RtgState(val elevator: Int = 0, val floors: List<Set<Component>>) {
+private data class RtgState(
+    val elevator: Int = 0,
+    val elementCount: Int,
+    val floors: List<BitSet>
+) {
+
+    companion object {
+        fun parse(input: String): RtgState {
+            val elements = regex.findAll(input)
+                .map { it.groupValues[1] }
+                .distinct()
+                .withIndex()
+                .map { it.value to it.index }
+                .toMap()
+
+            val floors = input.lines().map { line ->
+                val bitSet = BitSet(elements.size * 2)
+                regex.findAll(line).map { it.destructured }.forEach { (e, t) ->
+                    val index = elements[e]!!
+                    if (t == " generator") {
+                        bitSet.set(index * 2)
+                    } else {
+                        bitSet.set(index * 2 + 1)
+                    }
+                }
+                bitSet
+            }
+            return RtgState(floors = floors, elementCount = elements.size)
+        }
+    }
 
     override fun toString(): String = buildString {
         floors.asReversed().forEachIndexed { index, floor ->
@@ -53,33 +87,22 @@ private data class RtgState(val elevator: Int = 0, val floors: List<Set<Componen
         }
     }
 
-    sealed class Component {
-        abstract val type: String
-
-        data class Chip(override val type: String) : Component() {
-            override fun toString() = "$type chip"
-        }
-
-        data class Generator(override val type: String) : Component() {
-            override fun toString() = "$type generator"
-        }
-    }
-
     fun isDone(): Boolean = floors.dropLast(1).all { it.isEmpty() }
 
     fun nextSteps(): Set<RtgState> = buildSet {
         val current = floors[elevator]
         val toMove = current.let { it.subSets(1) + it.subSets(2) }.filter { it.isValid() }
 
-        toMove.forEach {
-            val nextCurrent = current - it
+        toMove.forEach { moving ->
+            val nextCurrent = current.clone().let { it as BitSet }.also { it.andNot(moving) }
 
             fun move(to: Int) {
                 if (to in floors.indices) {
                     val target = floors[to]
-                    val nextTarget = target + it
+                    val nextTarget = target.clone().let { it as BitSet }.also { it.or(moving) }
                     if (nextCurrent.isValid() && nextTarget.isValid()) this += RtgState(
                         elevator = to,
+                        elementCount = elementCount,
                         floors = floors.mapIndexed { index, floor ->
                             when (index) {
                                 elevator -> nextCurrent
@@ -96,67 +119,57 @@ private data class RtgState(val elevator: Int = 0, val floors: List<Set<Componen
         }
     }
 
-    private fun Set<Component>.isValid() = none { it is Component.Chip } ||
-            none { it is Component.Generator } ||
-            all { it !is Component.Chip || Component.Generator(it.type) in this }
+    private fun BitSet.isValid(): Boolean {
+        var index = nextSetBit(0)
+        var hasUnshieldedChip = false
+        var hasGenerator = false
+        while (index != -1) {
+            if (index % 2 == 0) {
+                hasGenerator = true
+            } else {
+                if (!get(index - 1)) hasUnshieldedChip = true
+            }
+            index = nextSetBit(index + 1)
+        }
+
+        return !hasGenerator || !hasUnshieldedChip
+    }
 }
 
-private val testInput1 = RtgState(
-    floors = listOf(
-        setOf(RtgState.Component.Chip("H"), RtgState.Component.Chip("L")),
-        setOf(RtgState.Component.Generator("H")),
-        setOf(RtgState.Component.Generator("L")),
-        setOf(),
-    )
-)
-//The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
-//The second floor contains a hydrogen generator.
-//The third floor contains a lithium generator.
-//The fourth floor contains nothing relevant.
+private val regex = """(\w+)(-compatible microchip| generator)""".toRegex()
 
-private val input = RtgState(
-    floors = listOf(
-        setOf(
-            RtgState.Component.Chip("Tm"),
-            RtgState.Component.Generator("Tm"),
-            RtgState.Component.Generator("Pu"),
-            RtgState.Component.Generator("Sr")
-        ),
-        setOf(RtgState.Component.Chip("Pu"), RtgState.Component.Chip("Sr")),
-        setOf(
-            RtgState.Component.Generator("Pm"),
-            RtgState.Component.Chip("Pm"),
-            RtgState.Component.Generator("Ru"),
-            RtgState.Component.Chip("Ru")
-        ),
-        setOf(),
-    )
-)
-//The first floor contains a thulium generator, a thulium-compatible microchip, a plutonium generator, and a strontium generator.
-//The second floor contains a plutonium-compatible microchip and a strontium-compatible microchip.
-//The third floor contains a promethium generator, a promethium-compatible microchip, a ruthenium generator, and a ruthenium-compatible microchip.
-//The fourth floor contains nothing relevant.
+private const val testInput1 =
+    """The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+The second floor contains a hydrogen generator.
+The third floor contains a lithium generator.
+The fourth floor contains nothing relevant."""
+
+private const val input =
+    """The first floor contains a thulium generator, a thulium-compatible microchip, a plutonium generator, and a strontium generator.
+The second floor contains a plutonium-compatible microchip and a strontium-compatible microchip.
+The third floor contains a promethium generator, a promethium-compatible microchip, a ruthenium generator, and a ruthenium-compatible microchip.
+The fourth floor contains nothing relevant."""
 
 private fun <T : Any> searchGraph(
     start: T,
     isDone: (T) -> Boolean,
     nextSteps: (T) -> Set<T>,
-    computePath: Boolean = false
+    computePath: Boolean = false,
+    heuristic: (T) -> Int = { 0 }
 ): Int {
     val dists = mutableMapOf(start to 0)
-    val queue = PriorityQueue<T>(compareBy { dists[it]!! })
+    val queue = PriorityQueue(compareBy<T> { dists[it]!! + heuristic(it) })
     queue += start
 
-    val paths = mutableMapOf(start to listOf(start))
+    val paths = mutableMapOf<T, T>()
     while (queue.isNotEmpty()) {
-        val current: T = queue.first()
+        val current: T = queue.remove()
 
         val dist = dists[current]!!
-        queue -= current
 
         if (isDone(current)) {
-            paths[current]?.forEach {
-                println(it)
+            if (computePath) {
+                reconstructPath(current, paths).forEach { println(it) }
             }
             return dist
         }
@@ -166,16 +179,28 @@ private fun <T : Any> searchGraph(
 
             if (dists[next] == null) {
                 dists[next] = nextDist
+
                 queue += next
-                if (computePath) paths[next] = paths[current]!! + next
+
+                if (computePath) paths[next] = current
             } else if (nextDist < dists[next]!!) {
                 dists[next] = nextDist
+
                 queue -= next
                 queue += next
-                if (computePath) paths[next] = paths[current]!! + next
+
+                if (computePath) paths[next] = current
             }
         }
     }
 
     return -1
 }
+
+private fun <T> reconstructPath(end: T, cameFrom: Map<T, T>): List<T> = buildList<T> {
+    var current: T? = end
+    while (current != null) {
+        this += current
+        current = cameFrom[current]
+    }
+}.asReversed()
