a636aebb5628f940e2710fd6cf2d418e6df15f18
ferinagy <frantisek.nagy.ml@gmail.com>
2023-12-18 10:22:25 +0100

    Clean up

--- src/main/kotlin/com/github/ferinagy/adventOfCode/GraphSearch.kt
+++ src/main/kotlin/com/github/ferinagy/adventOfCode/GraphSearch.kt
@@ -58,46 +58,14 @@ fun <T : Any> searchGraph(
     computePath: Boolean = false,
     heuristic: (T) -> Int = { 0 },
     allowDuplicatesInQueue: Boolean = true
-): Int {
-    val dists = mutableMapOf(start to 0)
-    val queue = PriorityQueue(compareBy<T> { dists[it]!! + heuristic(it) })
-    queue += start
-
-    val paths = mutableMapOf<T, T>()
-    while (queue.isNotEmpty()) {
-        val current: T = queue.remove()
-
-        val dist = dists[current]!!
-
-        if (isDone(current)) {
-            if (computePath) {
-                reconstructPath(current, paths).forEach { println(it) }
-            }
-            return dist
-        }
-
-        for ((next, stepSize) in nextSteps(current)) {
-            val nextDist = dist + stepSize
-
-            if (dists[next] == null || nextDist < dists[next]!!) {
-                dists[next] = nextDist
-
-                queue += next
-
-                if (computePath) paths[next] = current
-            } else if (nextDist < dists[next]!!) {
-                dists[next] = nextDist
-
-                if (!allowDuplicatesInQueue) queue -= next
-                queue += next
-
-                if (computePath) paths[next] = current
-            }
-        }
-    }
-
-    return -1
-}
+): Int = searchGraph(
+    startingSet = setOf(start),
+    isDone = isDone,
+    nextSteps = nextSteps,
+    computePath = computePath,
+    heuristic = heuristic,
+    allowDuplicatesInQueue = allowDuplicatesInQueue
+)
 
 fun <T> Set<T>.singleStep() = map { it to 1 }.toSet()
 
