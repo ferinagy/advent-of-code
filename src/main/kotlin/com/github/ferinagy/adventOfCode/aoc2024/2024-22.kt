5ddcff1d89eb6e462fdb79931b35b8affd51ceb1
ferinagy <frantisek.nagy.ml@gmail.com>
2024-12-22 21:38:46 +0100

    Improve 2024/22

--- src/main/kotlin/com/github/ferinagy/adventOfCode/aoc2024/2024-22.kt
+++ src/main/kotlin/com/github/ferinagy/adventOfCode/aoc2024/2024-22.kt
@@ -2,6 +2,7 @@ package com.github.ferinagy.adventOfCode.aoc2024
 
 import com.github.ferinagy.adventOfCode.println
 import com.github.ferinagy.adventOfCode.readInputLines
+import kotlin.time.measureTime
 
 fun main() {
     val input = readInputLines(2024, "22-input")
@@ -15,34 +16,38 @@ fun main() {
     println()
     println("Part2:")
     part2(test2).println()
-    part2(input).println()
+    measureTime {
+        part2(input).println()
+    }.println()
 }
 
 private fun part1(input: List<String>) = input.sumOf { get2000th(it.toUInt()).last().toULong() }
 
 
 private fun part2(input: List<String>): Int {
-    val maps = input.map {
+    val cache =  mutableMapOf<List<Int>, Int>()
+    input.forEach {
         val secrets = get2000th(it.toUInt())
         val prices = secrets.map { it.toInt() % 10 }
-        val diffs = prices.windowed(2).map { (a, b) -> (b % 10) - (a % 10) }
+        val diffs = prices.windowed(2).map { (a, b) -> b - a }
 
-        diffs.windowed(4).foldIndexed(mutableMapOf<List<Int>, Int>()) { index, acc, ints ->
-            acc.also { if (ints !in it) it[ints] = prices[index + 4] }
+        val seen = mutableSetOf<List<Int>>()
+        diffs.windowed(4).forEachIndexed { index, ints ->
+            if (ints !in seen) {
+                seen += ints
+                cache[ints] = cache.getOrDefault(ints, 0) + prices[index + 4]
+            }
         }
     }
-
-    return maps.flatMapTo(mutableSetOf()) { it.keys }.maxOf { seq ->
-        maps.sumOf { it.getOrDefault(seq, 0) }
-    }
+    return cache.maxOf { it.value }
 }
 
 private fun get2000th(secret: UInt): List<UInt> = generateSequence(secret) { nextSecret(it) }.take(2001).toList()
 
 private fun nextSecret(current: UInt): UInt {
-    val step1 = prune(mix(current * 64u, current))
-    val step2 = prune(mix(step1 / 32u, step1))
-    val step3 = prune(mix(step2 * 2048u, step2))
+    val step1 = prune(mix(current shl 6, current))
+    val step2 = prune(mix(step1 shr 5, step1))
+    val step3 = prune(mix(step2 shl 11, step2))
     return step3
 }
 
